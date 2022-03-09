10a173d45a6f2de7a6f570f8570b0720166d295c
KKimyeji <kyi4149@naver.com>
2022-03-09 19:32:52 +0900

    create a menu

--- Yeji.java
+++ Yeji.java
@@ -1,8 +1,11 @@
 package HelloYejiWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Heollo Yeji World");
-		System.out.println("I'm happy to meet you");
+		System.out.println("1. Add student");
+		System.out.println("2. Delete student");
+		System.out.println("3. Edit student");
+		System.out.println("4. view student");
+		System.out.println("5. Exit");
 		
 
 	}
