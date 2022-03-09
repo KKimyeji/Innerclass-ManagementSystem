221d7dd33c0bc408c8cb1cb2e0b04f63c6e1ac7c
KKimyeji <kyi4149@naver.com>
2022-03-10 00:15:02 +0900

    create a menu

---
+++ Hello/src/helloYeji/InnerclassManagement.java
@@ -0,0 +1,13 @@
+package helloYeji;
+
+class InnerclassManagement {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add student");
+		System.out.println("2. Delete student");
+		System.out.println("3.Edit student");
+		System.out.println("4. view student");
+		System.out.println("5. Exit");
+	}
+
+}
