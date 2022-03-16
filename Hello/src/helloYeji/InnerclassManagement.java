0b240136090ccf8c2c325e74504d653c54ce119c
KKimyeji <kyi4149@naver.com>
2022-03-16 18:17:00 +0900

    add Menu selection

--- Hello/src/helloYeji/InnerclassManagement.java
+++ Hello/src/helloYeji/InnerclassManagement.java
@@ -1,15 +1,49 @@
 package helloYeji;
 
+import java.util.Scanner;
+
 class InnerclassManagement {
 
 	public static void main(String[] args) {
-		System.out.println("1. Add student");
-		System.out.println("2. Delete student");
-		System.out.println("3. Edit student");
-		System.out.println("4. view student");
-		System.out.println("5. Exit");
-		System.out.println("This is helloYeji");
+		int num = 0;
+		Scanner input = new Scanner(System.in);
 		
+		while (num !=6) {
+		System.out.println("1. Add class");
+		System.out.println("2. Delete class");
+		System.out.println("3. Edit class");
+		System.out.println("4. view class");
+		System.out.println("5. show a Menu");
+		System.out.println("6. Exit");
+		System.out.println("choose only one number between 1-6:");
+		num = input.nextInt();
+		switch(num) {
+		case 1:
+			System.out.println("classname:");
+			String classname = input.next();
+			break;
+		case 2:
+			System.out.println("classplace:");
+		    String classplace = input.next();
+		    break;
+		case 3:
+			System.out.println("clasID:");
+	        String classID = input.next();
+	        break;
+			
+		case 4:
+			System.out.println("classTime:");
+            String classTime = input.next();
+            break;
+			
+		}
+		
+			
+		}
+		
+		}
 	}
 
-}
+	
+
+
