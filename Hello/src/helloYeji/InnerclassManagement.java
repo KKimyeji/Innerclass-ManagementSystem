e6d659b2e90fdbe110596bc6200cc1b38de1988d
gim-yeji <gim-yeji@localhost>
2022-03-29 23:27:39 +0900

    0329

--- Hello/src/helloYeji/InnerclassManagement.java
+++ Hello/src/helloYeji/InnerclassManagement.java
@@ -1,68 +1,59 @@
 package helloYeji;
 
 import java.util.Scanner;
-
+    
 class InnerclassManagement {
-
-	public static void main(String[] args) {
-		int num =0;
-		Scanner input = new Scanner(System.in);
-		num = input.nextInt();
-		System.out.print("num"+ num);
-		
-		while (num !=6) {
-		System.out.println("1. Add class");
-		System.out.println("2. Delete class");
-		System.out.println("3. Edit class");
-		System.out.println("4. view class");
-		System.out.println("5. show a Menu");
-		System.out.println("6. Exit");
-		System.out.println("choose only one number between 1-6:");
-		
-		
-		if (num == 1) {
-		      Addclass();
-		 }
-		 else if (num == 2) {
-		      Deleteclass();
-		 
-		}
-		 else if (num == 3) {
-		      Editclass();
-		
-			
-		}
-		 else if (num == 4) {
-		       Viewclass();
-		
-		 }
-		 
-		}
+	Innerclass Innerclass;
+	Scanner input;	
+	InnerclassManagement(Scanner input){
+		this.input = input;
 	}
-	 
-	    public static void Addclass() {
-		Scanner input = new Scanner(System.in);
+	
+	    public  void Addclass() {
+	    	Innerclass = new Innerclass();
 			System.out.println("classname");
-			String classname = input.nextLine();
+			Innerclass. classname = input.next();
+			System.out.println("classplace");
+			Innerclass. classplace = input.next();
+			System.out.println("classID");
+			Innerclass. classID = input.nextInt();
+			System.out.println("classtime");
+			Innerclass. classtime = input.next();
 		
 	}
-	    public static void Deleteclass() {
-	    Scanner input = new Scanner(System.in);
-				System.out.println("classplace");
-				String classplace = input.nextLine();
+	    public  void Deleteclass() {
+				System.out.println("classname");
+				String classname = input.next();
+				
+				if(Innerclass == null) {
+					System.out.println("The classname doesn't exist");
+					return;
+				}
 			
+				if(Innerclass.classname == classname) {
+				Innerclass = null;
+				System.out.println("The classname dosen't have yet");
+				
+			}
 	    }
 
-	    public static void Editclass() {
-	    	Scanner input = new Scanner(System.in) ;
-				System.out.println("classID");
-				int classID = input.nextInt();
+	    public  void Editclass() {
+				System.out.println("classname");
+				String classname = input.next();
+				if(Innerclass.classname == classname) {
+					System.out.println("The classname has been edited:"+classname);
+					
+				}
 			
 	    }
-	    public static void Viewclass() {
-	    	Scanner input = new Scanner(System.in);
-				System.out.println("classtime");
-				String classtime = input.nextLine();
+	    public  void Viewclass() {
+				System.out.println("classname");
+				String classname = input.next();
+				if(Innerclass.classname == classname) {
+					Innerclass.printInfo();
+				}
+
+				
 			
 	    }
 }
